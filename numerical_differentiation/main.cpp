d1c9ed5db3f3bf59dae59e449350be0f095fa21c
Dmitrii <deenixie@gmail.com>
2023-09-22 21:54:42 +0300

    make every index in loops unsigned int

--- numerical_differentiation/main.cpp
+++ numerical_differentiation/main.cpp
@@ -7,7 +7,7 @@ int main() {
                                       1e-6,  1e-7,  1e-8,  1e-9, 1e-10, 1e-11,
                                       1e-12, 1e-13, 1e-14, 1e-15};
     const std::array<double, N> points{-1, 1};
-    for (int j = 0; j < 16; ++j) {
+    for (unsigned int j = 0; j < 16; ++j) {
         std::cout << std::setprecision(16)
                   << exp_derivative<double, 2>(x_0, step[j], points)
                   << std::endl;
@@ -21,7 +21,7 @@ int main() {
     const unsigned int num = 10000;
     std::vector<double> x = linspace(start, end, num);
     data.open("dataN3.txt");
-    for (int i = 0; i < num; ++i) {
+    for (unsigned int i = 0; i < num; ++i) {
         data << x[i] << " "
              << std::log(std::abs(
                     exp_derivative<double, 3>(x_0, std::exp(x[i]), {-1, 1, 2}) -
@@ -31,7 +31,7 @@ int main() {
     data.close();
 
     data.open("dataN4.txt");
-    for (int i = 0; i < num; ++i) {
+    for (unsigned int i = 0; i < num; ++i) {
         data << x[i] << " "
              << std::log(std::abs(exp_derivative<double, 4>(x_0, std::exp(x[i]),
                                                             {-2, -1, 1, 2}) -
@@ -41,7 +41,7 @@ int main() {
     data.close();
 
     data.open("dataN5.txt");
-    for (int i = 0; i < num; ++i) {
+    for (unsigned int i = 0; i < num; ++i) {
         data << x[i] << " "
              << std::log(std::abs(exp_derivative<double, 5>(x_0, std::exp(x[i]),
                                                             {-2, -1, 1, 2, 3}) -
--- numerical_differentiation/numerical_differentiation.h
+++ numerical_differentiation/numerical_differentiation.h
@@ -10,7 +10,7 @@ std::vector<double> linspace(const double start, const double end,
     std::vector<double> res;
     double delta = (end - start) / N;
     ;
-    for (int i = 0; i < N; ++i) {
+    for (unsigned int i = 0; i < N; ++i) {
         res.push_back(start + i * delta);
     }
     return res;
@@ -29,18 +29,18 @@ calcDerivativeCoef(const std::array<RealType, N> &points) noexcept {
     b(1) = 1;
     Eigen::Matrix<RealType, N + 1, N + 1> A =
         Eigen::Matrix<RealType, N + 1, N + 1>::Zero(N + 1, N + 1);
-    for (int i = 0; i <= N; ++i) {
+    for (unsigned int i = 0; i <= N; ++i) {
         A(0, i) = 1;
     }
-    for (int j = 1; j <= N; ++j) {
-        for (int i = 0; i < N; ++i) {
+    for (unsigned int j = 1; j <= N; ++j) {
+        for (unsigned int i = 0; i < N; ++i) {
             A(j, i + 1) = A(j - 1, i + 1) * points[i] / j;
         }
     }
     Eigen::Matrix<RealType, N + 1, 1> v_res = A.colPivHouseholderQr().solve(b);
 
     std::array<RealType, N> res;
-    for (int i = 0; i < N; ++i) {
+    for (unsigned int i = 0; i < N; ++i) {
         res[i] = v_res[i + 1];
     }
     return (DerivativeCoef<RealType, N>){v_res[0], res};
@@ -51,7 +51,7 @@ RealType exp_derivative(const RealType x_0, const RealType h,
                         const std::array<RealType, N> &points) {
     DerivativeCoef<RealType, N> coefs = calcDerivativeCoef<RealType, N>(points);
     RealType res = std::exp(x_0) * coefs.centralCoef / h;
-    for (int i = 0; i < points.size(); ++i) {
+    for (unsigned int i = 0; i < points.size(); ++i) {
         res += std::exp(x_0 + points[i] * h) * coefs.otherCoefs[i] / h;
     }
     return res;
