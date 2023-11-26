999278fdfabbe86a7e201a27105b34f2a517ea13
Dmitrii <deenixie@gmail.com>
2023-11-27 00:57:35 +0300

    add main.cpp for bdf lab

--- source/bdf/main.cpp
+++ source/bdf/main.cpp
@@ -25,7 +25,6 @@ std::array<RealType, N> powof10(const std::array<double, N> &x) {
 }
 
 int main() {
-    IntegrationParameters param;
     std::ofstream data;
     // data.open("data_cube.txt");
     // const Eigen::Vector<double, 1> a{0};
@@ -42,18 +41,37 @@ int main() {
     //     data << std::endl;
     // }
     // data.close();
-    data.open("data_oscillator.txt");
-    const Eigen::Vector<double, 2> a{0, 1};
-    const Oscillator::StateAndArg initCond{a, 0};
-    const std::array<double, 12> steps =
-        powof10<double, 12>(linspace<double, 12>(-6, 1));
-    for (unsigned int i = 0; i < 12; ++i) {
-        auto res = integrate<BDF4, Oscillator, RungeKutta::RK4Table>(
-            initCond, 5, {steps[i], 0.01, 100}, oscillator);
-        for (const auto &element : res) {
-            data << std::setprecision(16) << element.arg << " "
-                 << element.state[0] << " " << element.state[1] <<std::endl;
-        }
-        data << std::endl;
+    // data.open("data_oscillator.txt");
+    // const Eigen::Vector<double, 2> a{0, 1};
+    // const Oscillator::StateAndArg initCond{a, 0};
+    // const std::array<double, 12> steps =
+    //     powof10<double, 12>(linspace<double, 12>(-6, 1));
+    // for (unsigned int i = 0; i < 12; ++i) {
+    //     auto res = integrate<BDF4, Oscillator, RungeKutta::RK4Table>(
+    //         initCond, 5, {steps[i], 0.01, 100}, oscillator);
+    //     for (const auto &element : res) {
+    //         data << std::setprecision(16) << element.arg << " "
+    //              << element.state[0] << " " << element.state[1] <<std::endl;
+    //     }
+    //     data << std::endl;
+    // }
+    data.open("earth_data.txt");
+    EarthOrbit earth;
+    const Eigen::Vector<double, 6> a{
+        6800, 0, M_PI_2, 0,
+        0.00116323529411764705882352941176, 0.00116323529411764705882352941176};
+    const EarthOrbit::StateAndArg initCond{a, 0};
+    auto res = integrate<BDF4, EarthOrbit, RungeKutta::RK4Table>(
+        initCond, 5000, {0.01, 1e-5, 10000}, earth);
+    for (const auto &element : res) {
+        data << element.arg << " "
+             << element.state[0] * std::cos(element.state[1]) *
+                    std::sin(element.state[2])
+             << " "
+             << element.state[0] * std::sin(element.state[1]) *
+                    std::sin(element.state[2])
+             << " " << element.state[0] * std::cos(element.state[2])
+             << std::endl;
     }
+    data.close();
 }
